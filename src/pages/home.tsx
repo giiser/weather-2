02162eab1f11856b5a1a32e84a3cc9c0a377f443
sergii.ignatov <s.ignatov92@gmail.com>
2025-11-29 21:34:12 +0200

    forgot to add footer to the home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header.tsx";
+import Footer from "../components/Footer.tsx";
 
 const HomePage = () => {
     return (
         <>
             <Header headline={"appTitle"} />
+            <Footer />
         </>
     )
 }
